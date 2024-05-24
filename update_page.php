d99a7ec759ead72e7e1a61e2f8b40495e4d2eef5
Ruben Omar Aguero <93364103+omaraguero@users.noreply.github.com>
2024-05-24 15:58:12 -0500

    Review for new video

--- update_page.php
+++ update_page.php
@@ -1,7 +1,8 @@
-<?php include('includes/header.php'); ?>
-<?php include('includes/dbcon.php'); ?>
+<?php include('includes/header.php');?>
+<?php include('includes/db_connection.php');?>
 
-<?php 
+
+<?php
 
     if(isset($_GET['id'])){
         $id = $_GET['id'];
@@ -14,61 +15,52 @@
             die("query failed".mysqli_error());
         }else{
             $row = mysqli_fetch_assoc($result);
+        }
+    }
 
+    if(isset($_POST['update_students'])){
+
+        if(isset($_GET['id_new'])){
+            $idnew = $_GET['id_new'];
         }
 
+        $fname = $_POST['f_name'];
+        $lname = $_POST['l_name'];
+        $age = $_POST['age'];
+
+        if($fname == "" || empty($fname) || $lname == "" || empty($lname) || $age == "" || empty($age) ){
+            header('location:index.php?message=You need to fill the values');
+        }else{
+
+            $query = "update `students` set `firstname` = '$fname', `lastname` = '$lname', `age` = '$age' where `id` = '$idnew' ";
+
+            $result = mysqli_query($connection, $query);
+
+            if(!$result){
+                die("query failed".mysqli_error());
+            }else{
+                header('location:index.php?update_msg=You have successfully update the data');
+            }
+        }
     }
 
     ?>
 
-        <?php
-
-            if(isset($_POST['update_students'])){
-                
-                if(isset($_GET['id_new'])){
-                    $idnew = $_GET['id_new'];
-                }
-
-                $fname = $_POST['f_name'];
-                $lname = $_POST['l_name'];
-                $age = $_POST['age'];
-
-                $query = "update `students` set `first_name` = '$fname', `last_name` = '$lname' , `age` = '$age' where `id` = '$idnew' " ;
-                
-                $result = mysqli_query($connection, $query);
-
-                if(!$result){
-                    die("query failed".mysqli_error());
-                }else{
-                    header('location:index.php?update_msg=You have successfully update the data.');
-                }
-        
-
-
-
-            }
-
-
-        ?>
-
-
 <form action="update_page.php?id_new=<?php echo $id; ?>" method="post">
-        <div class="form-group">
-            <label for="f_name">First Name</label>
-            <input type="text" name="f_name" class="form-control" value="<?php echo $row['first_name']?>">
-        </div>
-        <div class="form-group">
-            <label for="l_name">Last Name</label>
-            <input type="text" name="l_name" class="form-control" value="<?php echo $row['last_name']?>">
-        </div>
-        <div class="form-group">
-            <label for="age">Age</label>
-            <input type="text" name="age" class="form-control" value="<?php echo $row['age']?>">
-            <input type="submit" class="btn btn-success mt-4" name="update_students" value="Update">                   
-        </div>                        
+    <div class="form-group">
+        <label for="f_name">First Name</label>
+        <input type="text" name="f_name" class="form-control" value="<?php echo $row['firstname']?>">
+    </div>
+    <div class="form-group">
+        <label for="l_name">Last Name</label>
+        <input type="text" name="l_name" class="form-control" value="<?php echo $row['lastname']?>">
+    </div>
+    <div class="form-group">
+        <label for="age">Age</label>
+        <input type="text" name="age" class="form-control" value="<?php echo $row['age']?>">
+        <input type="submit" class="btn btn-success mt-4" name="update_students" value="Update">
+    </div>
 </form>
-
-
 
 
 
